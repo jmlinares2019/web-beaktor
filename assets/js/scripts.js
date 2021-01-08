10206f7dbd62f627d8958deaa4627e720aaa0741
jmlinares2019 <jlinaresmar@gmail.com>
2021-01-08 12:57:11 +0100

    Owl Carousel responsive options in script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,6 +8,24 @@ $(document).ready(function(){
 
 // Owl Carousel
 
+var owlOptions = {
+  loop: true,
+  center: true,
+  //margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0:{
+        items:1
+    },
+    800:{
+        items:2
+    },
+    1200:{
+        items:3
+    }
+}
+}
+
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel(owlOptions);
 });
